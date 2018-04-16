3fce7920e333616d0c932ca085def926a128b772
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2018-04-16 13:57:54 +0600

    update merge sort algorithm

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -1,49 +1,56 @@
+from copy import copy
+
+
 def merge_sort(numbers):         
-    m_sort(numbers, [None] * len(numbers), 0, len(numbers)-1)
+    copy_numbers = copy(numbers)
+    swap_count = m_sort(copy_numbers, [None] * len(numbers), 0, len(numbers)-1)
+    print (swap_count)
+    return copy_numbers
 
 def m_sort(numbers, temp, left, right):    
     if left >= right:
         temp[left] = numbers[left]                  
-        return                                          
+        return 0                                        
     mid = (right+left)//2
-    m_sort(numbers, temp, left, mid)
-    m_sort(numbers, temp, mid+1, right)
-    merge(numbers, temp, left, mid+1, right)
+    swap_count1 = m_sort(numbers, temp, left, mid)
+    swap_count2 = m_sort(numbers, temp, mid+1, right)
+    swap_count3 =  merge(numbers, temp, left, mid+1, right)
+    return  swap_count1 + swap_count2 + swap_count3
 
 def merge(numbers, temp, left, mid, right):
     left_end = mid - 1                       
     tmp_pos = left                          
-    num_elements = right - left  + 1        
+    num_elements = right - left  + 1
+    swap_count = 0 
+    i, j = 0, 0      
     while(left <= left_end and mid <= right):
         if numbers[left] <= numbers[mid]:      
             temp[tmp_pos] = numbers[left]        
             tmp_pos += 1                    
-            left += 1                       
+            left += 1 
+            swap_count += j
         else:         
+            j += 1
             temp[tmp_pos] = numbers[mid]
             tmp_pos += 1                
-            mid += 1                    
-    while left <= left_end:
-        temp[tmp_pos] = numbers[left]
-        left += 1                    
-        tmp_pos += 1                 
-    while mid <= right:
-        temp[tmp_pos] = numbers[mid]
-        mid += 1                    
-        tmp_pos += 1                
-    for i in range(num_elements):
-        numbers[right] = temp[right]
-        right -= 1
+            mid += 1
+    if left <= left_end:
+        temp[tmp_pos: tmp_pos + left_end - left] = numbers[left: left_end + 1]
+        tmp_pos += (left_end - left)
+    if mid <= right:
+        temp[tmp_pos: tmp_pos + right - mid] = numbers[mid: right + 1]
+             
+    numbers[right - num_elements + 1: right + 1] = temp[right - num_elements + 1: right + 1]
+    return swap_count
 
 if __name__ == "__main__":
     import sys
-    print (sys.argv)
     if len(sys.argv) > 1:
         numbers = list(map(int, sys.argv[1:]))
-        merge_sort(numbers)
-        print(numbers)
+        sorted_list = merge_sort(numbers)
+        print(sorted_list)
     else:
-        numbers = input("Please enter space seperated numbers")
+        numbers = input("Please enter space seperated numbers: ")
         numbers = list(map(int, numbers.strip().split(' ')))
-        merge_sort(numbers)
-        print (numbers)
+        sorted_list = merge_sort(numbers)
+        print (sorted_list)
